e4e86221afecb5a992351f14fb7081eef5fd744a
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-09-24 10:57:55 +0300

    fix: move mappers to controller-level

--- src/main/java/com/software/modsen/rideservice/controller/RideController.java
+++ src/main/java/com/software/modsen/rideservice/controller/RideController.java
@@ -3,6 +3,8 @@ package com.software.modsen.rideservice.controller;
 import com.software.modsen.rideservice.dto.request.RideRequest;
 import com.software.modsen.rideservice.dto.response.RideListResponse;
 import com.software.modsen.rideservice.dto.response.RideResponse;
+import com.software.modsen.rideservice.mapper.RideMapper;
+import com.software.modsen.rideservice.model.Ride;
 import com.software.modsen.rideservice.service.RideService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,49 +17,63 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/rides")
 @RequiredArgsConstructor
 public class RideController {
     private final RideService rideService;
+    private final RideMapper rideMapper;
 
     @GetMapping
-    public ResponseEntity<RideListResponse> getAllRides(){
-        return ResponseEntity.ok().body(rideService.gelAllRides());
+    public ResponseEntity<RideListResponse> getAllRides() {
+        return ResponseEntity.ok(
+                new RideListResponse(rideService.gelAllRides()
+                        .stream()
+                        .map(rideMapper::toResponse)
+                        .collect(Collectors.toList())));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<RideResponse> getRideById(@PathVariable Long id){
-        return ResponseEntity.ok().body(rideService.getRide(id));
+    public ResponseEntity<RideResponse> getRideById(@PathVariable Long id) {
+        return ResponseEntity.ok(rideMapper.toResponse(rideService.getRide(id)));
     }
 
     @GetMapping("/free")
-    public ResponseEntity<RideListResponse> gelAllFreeRides(){
-        return ResponseEntity.ok().body(rideService.getAllCreatedRides());
+    public ResponseEntity<RideListResponse> gelAllFreeRides() {
+        return ResponseEntity.ok(
+                new RideListResponse(rideService.getAllCreatedRides()
+                        .stream()
+                        .map(rideMapper::toResponse)
+                        .collect(Collectors.toList())));
     }
 
     @PostMapping
-    public ResponseEntity<RideResponse> createRide(@RequestBody RideRequest rideRequest){
-        return ResponseEntity.status(HttpStatus.CREATED).body(rideService.createRide(rideRequest));
+    public ResponseEntity<RideResponse> createRide(@RequestBody RideRequest rideRequest) {
+        Ride ride = rideMapper.toModel(rideRequest);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(rideMapper.toResponse(rideService.createRide(ride)));
     }
 
     @PutMapping("/{id}/cancel")
-    public ResponseEntity<RideResponse> cancelRide(@PathVariable Long id){
-        return ResponseEntity.ok().body(rideService.cancelRide(id));
+    public ResponseEntity<RideResponse> cancelRide(@PathVariable Long id) {
+        return ResponseEntity.ok(rideMapper.toResponse(rideService.cancelRide(id)));
     }
 
     @PutMapping("/{id}/accept")
-    public ResponseEntity<RideResponse> acceptRide(@PathVariable Long id, @RequestBody Long driverId){
-        return ResponseEntity.ok().body(rideService.acceptRide(id, driverId));
+    public ResponseEntity<RideResponse> acceptRide(@PathVariable Long id, @RequestBody Long driverId) {
+        return ResponseEntity.ok(rideMapper.toResponse(rideService.acceptRide(id, driverId)));
     }
 
     @PutMapping("/{id}/start")
-    public ResponseEntity<RideResponse> startRide(@PathVariable Long id){
-        return ResponseEntity.ok().body(rideService.startRide(id));
+    public ResponseEntity<RideResponse> startRide(@PathVariable Long id) {
+        return ResponseEntity.ok(rideMapper.toResponse(rideService.startRide(id)));
     }
 
     @PutMapping("/{id}/finish")
-    public ResponseEntity<RideResponse> finishRide(@PathVariable Long id){
-        return ResponseEntity.ok().body(rideService.finishRide(id));
+    public ResponseEntity<RideResponse> finishRide(@PathVariable Long id) {
+        return ResponseEntity.ok(rideMapper.toResponse(rideService.finishRide(id)));
     }
 }
--- src/main/java/com/software/modsen/rideservice/service/RideService.java
+++ src/main/java/com/software/modsen/rideservice/service/RideService.java
@@ -1,10 +1,6 @@
 package com.software.modsen.rideservice.service;
 
-import com.software.modsen.rideservice.dto.request.RideRequest;
-import com.software.modsen.rideservice.dto.response.RideListResponse;
-import com.software.modsen.rideservice.dto.response.RideResponse;
 import com.software.modsen.rideservice.exception.RideNotFoundException;
-import com.software.modsen.rideservice.mapper.RideMapper;
 import com.software.modsen.rideservice.model.Ride;
 import com.software.modsen.rideservice.model.Status;
 import com.software.modsen.rideservice.repository.RideRepository;
@@ -13,61 +9,54 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RideService {
     private final RideRepository rideRepository;
-    private final RideMapper rideMapper;
 
-    public RideResponse getRide(Long id) {
-        Ride ride = getByIdOrElseThrow(id);
-        return rideMapper.toResponse(ride);
+    public Ride getRide(Long id) {
+        return getByIdOrElseThrow(id);
     }
 
-    public RideListResponse gelAllRides() {
-        return new RideListResponse(rideRepository.findAll().stream()
-                .map(rideMapper::toResponse)
-                .collect(Collectors.toList()));
+    public List<Ride> gelAllRides() {
+        return rideRepository.findAll();
     }
 
-    public RideListResponse getAllCreatedRides() {
-        return new RideListResponse(rideRepository.getRidesByStatusIs(Status.CREATED).stream()
-                .map(rideMapper::toResponse)
-                .collect(Collectors.toList()));
+    public List<Ride> getAllCreatedRides() {
+        return rideRepository.getRidesByStatusIs(Status.CREATED);
     }
 
-    public RideResponse createRide(RideRequest request) {
-        Ride ride = rideMapper.toModel(request);
+    public Ride createRide(Ride ride) {
         ride.setStatus(Status.CREATED);
         ride.setDateTimeCreate(LocalDateTime.now());
-        return rideMapper.toResponse(rideRepository.save(ride));
+        return rideRepository.save(ride);
     }
 
-    public RideResponse acceptRide(Long id, Long driverId) {
+    public Ride acceptRide(Long id, Long driverId) {
         Ride ride = getByIdOrElseThrow(id);
         ride.setDriverId(driverId);
         ride.setStatus(Status.ACCEPTED);
-        return rideMapper.toResponse(rideRepository.save(ride));
+        return rideRepository.save(ride);
     }
 
-    public RideResponse cancelRide(Long id) {
+    public Ride cancelRide(Long id) {
         Ride ride = getByIdOrElseThrow(id);
         ride.setStatus(Status.CANCELED);
-        return rideMapper.toResponse(rideRepository.save(ride));
+        return rideRepository.save(ride);
     }
 
-    public RideResponse finishRide(Long id) {
+    public Ride finishRide(Long id) {
         Ride ride = getByIdOrElseThrow(id);
         ride.setStatus(Status.FINISHED);
-        return rideMapper.toResponse(rideRepository.save(ride));
+        return rideRepository.save(ride);
     }
 
-    public RideResponse startRide(Long id) {
+    public Ride startRide(Long id) {
         Ride ride = getByIdOrElseThrow(id);
         ride.setStatus(Status.ACTIVE);
-        return rideMapper.toResponse(rideRepository.save(ride));
+        return rideRepository.save(ride);
     }
 
     private Ride getByIdOrElseThrow(Long id) {
