e51025b77583232b482924ce03d1ac9e3f269dc6
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-09-24 10:59:50 +0300

    feat: add eureka

--- src/main/java/com/software/modsen/rideservice/util/ExceptionMessages.java
+++ src/main/java/com/software/modsen/rideservice/util/ExceptionMessages.java
@@ -1,8 +1,5 @@
 package com.software.modsen.rideservice.util;
 
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
 public class ExceptionMessages {
     public static final String RIDE_NOT_FOUND_EXCEPTION = "ride with id '%s' not found";
 }
