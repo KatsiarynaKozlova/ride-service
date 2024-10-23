a56ba4d8b6efa11462695c77f15da369aae9ddb6
Katsiaryna Kozlova <katekozlova1411@gmail.com>
2024-10-23 15:46:46 +0300

    feat: add log info

---
+++ src/main/java/com/software/modsen/rideservice/util/LogInfoMessages.java
@@ -0,0 +1,9 @@
+package com.software.modsen.rideservice.util;
+
+public class LogInfoMessages {
+    public static final String GET_RIDE_BY_ID = "get ride with id '%s'";
+    public static final String GET_LIST_OF_RIDES = "get ride list";
+    public static final String GET_LIST_OF_CREATED_RIDES = "get created rides";
+    public static final String CREATE_RIDE_WITH_ID = "create ride with id '%s'";
+    public static final String UPDATE_RIDE_STATUS = "updated ride status of ride with id '%s'";
+}
